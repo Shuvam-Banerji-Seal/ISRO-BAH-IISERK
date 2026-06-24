d00ec9057f696019db2909dcaf0c5c5f555fd244
kingkamra1729 <kamrarama@gmail.com>
2026-06-25 00:15:06 +0530

    Update solexs.sh

--- solexs.sh
+++ solexs.sh
@@ -5,7 +5,7 @@
 #Caution: There are session download limits, request rate limit and session timeouts in place, etc.
 #	Violations may lead to blocking. Use script to ease the manual data download efforts but do not load the server.
 
-cookies="FGTServer=03DE191863F4388C06A7AAAF7E0136FBD15060DF21FA637D82A675307CD5BF28BF8658CAFD950178C9994D;FGTServer=03DE191863F4388C06A7AAAF7E0136FBD15060DF21FA637D82A675307CD5BF28BF8658CAFD950178C9994D;JSESSIONID=aa65b5546a91cf48c4304f75c09b;FGTServer=03DE191863F4388C06A7AAAF7E0136FBD15060DF21FA637D82A675307CD5BF28BF8658CAFD950178C9994D;JSESSIONID=aa65482540695f403abc4503b47d;OAuth_Token_Request_State=15008e9c-2c12-41df-84fe-dc5426151f0c;"
+cookies=""
 urlPrefix="https://pradan1.issdc.gov.in"
 #proxyOptions are required if your organization uses proxy to connect to Internet.
 #proxyOptions="-e use_proxy=yes -e https_proxy=127.0.0.1:8080"
