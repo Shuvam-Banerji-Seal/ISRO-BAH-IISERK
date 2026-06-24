4145f93688c46f4efed9e36471ab31e3f3f67ef8
kingkamra1729 <kamrarama@gmail.com>
2026-06-25 00:14:12 +0530

    Update hel1os_2026Jun22T091248290.sh
    Cleared cookie variables for security reasons.

--- hel1os_2026Jun22T091248290.sh
+++ hel1os_2026Jun22T091248290.sh
@@ -5,7 +5,7 @@
 #Caution: There are session download limits, request rate limit and session timeouts in place, etc.
 #	Violations may lead to blocking. Use script to ease the manual data download efforts but do not load the server.
 
-cookies="FGTServer=03DE191863F4388C06A7AAAF7E0136FBD15060DF21FA637D82A675307CD5BF28BF8658CAFD950178C9994D;JSESSIONID=e998f36a6a282ea358264e731e2b;OAuth_Token_Request_State=4d31ea46-8efc-4baf-803a-80e478b9a39c;JSESSIONID=e99c60e519f20ad4d6fcea8ff316;FGTServer=03DE191863F4388C06A7AAAF7E0136FBD15060DF21FA637D82A675307CD5BF28BF8658CAFD950178CB994D;"
+cookies=""
 urlPrefix="https://pradan1.issdc.gov.in"
 #proxyOptions are required if your organization uses proxy to connect to Internet.
 #proxyOptions="-e use_proxy=yes -e https_proxy=127.0.0.1:8080"
